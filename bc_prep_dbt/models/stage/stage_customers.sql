efaef3ad8a88fb0c0a957b93a65ee39f00548779
cord.siebke <cord.siebke@lm190306.localdomain>
2020-06-09 18:36:31 +0200

    added to produce a table as mart_cohorts

--- bc_prep_dbt/models/stage/stage_customers.sql
+++ bc_prep_dbt/models/stage/stage_customers.sql
@@ -1,5 +1,7 @@
 WITH SOURCE AS
   ( SELECT *
-   FROM {{ ref('input_customers') }} )
+   FROM {{ ref('input_customers') }} 
+   WHERE (customer_migration_flag = 'f' AND customer_merge_flag = 'f')
+   )
 SELECT *
 FROM SOURCE
--- bc_prep_dbt/models/stage/stage_orders.sql
+++ bc_prep_dbt/models/stage/stage_orders.sql
@@ -1,5 +1,7 @@
 WITH SOURCE AS
   ( SELECT *
-   FROM {{ ref('input_orders') }} )
+   FROM {{ ref('input_orders') }} 
+   WHERE order_migration_flag = 'f'
+   )
 SELECT *
 FROM SOURCE
